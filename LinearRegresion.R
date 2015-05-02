5b025e9ace30f9acd2850ecfbe74b41ecfc207d9
Bernat417 <bernat.huguet@gmail.com>
2015-05-02 23:53:59 +0200

    New

--- LinearRegresion.R
+++ LinearRegresion.R
@@ -4,9 +4,10 @@ Train = read.csv("NYTimesBlogTrain.csv", stringsAsFactors=FALSE)
 Test = read.csv("NYTimesBlogTest.csv", stringsAsFactors=FALSE)
 
 #Generate the Linear Regresion and predict the results
-SimpleLR = glm(Popular ~ WordCount, data=NewsTrain, family=binomial)
-Prediction = predict(SimpleLR, newdata=NewsTest, type="response")
+SimpleLR = glm(Popular ~ WordCount, data=Train, family=binomial)
+Prediction = predict(SimpleLR, newdata=Test, type="response")
 
 #Print the result
-Submission = data.frame(UniqueID = NewsTest$UniqueID, Probability1 = Prediction)
+Submission = data.frame(UniqueID = Test$UniqueID, Probability1 = Prediction)
 write.csv(Submission, "SimpleLR.csv", row.names=FALSE)
+
